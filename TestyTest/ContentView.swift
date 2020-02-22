54a3d669d173ba18fde46595b67199619f7aeacd
Gabriel Perdue <gnperdue@me.com>
2020-02-21 18:56:11 -0600

    Initial Commit

---
+++ TestyTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestyTest
+//
+//  Created by Gabriel Perdue on 2/21/20.
+//  Copyright © 2020 Gabriel Perdue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
