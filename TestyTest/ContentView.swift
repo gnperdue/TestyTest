abfaeca5a672f9b6ffa010057131fe87f1195b7e
Gabriel Perdue <gnperdue@me.com>
2020-05-27 06:30:42 -0500

    every slightly fancier plotting -- add some helper functions to move towards axes markers

--- TestyTest/ContentView.swift
+++ TestyTest/ContentView.swift
@@ -12,24 +12,53 @@ import SwiftUI
 struct ContentView: View {
   let keys = ExampleGameData().scoreDict.keys.sorted()
   let scoreDict = ExampleGameData().scoreDict
-  let highScore = ExampleGameData().highScore
+  let highScore = 50
+
+  // how much horizontal space does each "day" in the set of scores take?
+  // TODO -- adjust to actual date span instead of just list of days
+  func dayWidth(_ width: CGFloat, count: Int) -> CGFloat {
+    width / CGFloat(count)
+  }
+
+  // how much vertical space does each score take?
+  func scoreHeight(_ height: CGFloat, range: Int) -> CGFloat {
+    height / CGFloat(range)
+  }
+  
+  func dayOffset(_ day: Int, dWidth: CGFloat) -> CGFloat {
+    CGFloat(day) * dWidth
+  }
+
+  func scoreOffset(_ score: Double, scoreHeight: CGFloat) -> CGFloat {
+    CGFloat(score + 1) * scoreHeight
+  }
+  
+  func scoreLabelOffset(_ line: Int, height: CGFloat) -> CGFloat {
+    height - self.scoreOffset(
+      Double(line * 10),
+      scoreHeight: self.scoreHeight(height, range: self.highScore))
+  }
   
   var body: some View {
     
     GeometryReader { reader in
-      
+
       ForEach(self.keys.indices[0..<self.keys.count-1]) { idx in
         Path { p in
-          let xScaleFactor = reader.size.width / CGFloat(self.keys.count + 1)
-          let yScaleFactor = reader.size.height / CGFloat(self.highScore * 1.1)
-          let startPoint = CGPoint(
-            x: CGFloat(idx + 1) * xScaleFactor,
-            y: reader.size.height -
-              CGFloat(self.scoreDict[self.keys[idx]]!) * yScaleFactor)
-          let endPoint = CGPoint(
-            x: CGFloat(idx + 2) * xScaleFactor,
-            y: reader.size.height -
-              CGFloat(self.scoreDict[self.keys[idx + 1]]!) * yScaleFactor)
+          let dWidth = self.dayWidth(reader.size.width,
+                                     count: self.keys.count + 1)
+          let sHeight = self.scoreHeight(reader.size.height,
+                                         range: self.highScore)
+          let dStart = self.dayOffset(idx + 1, dWidth: dWidth)
+          let dStop = self.dayOffset(idx + 2, dWidth: dWidth)
+          let scoreStart = self.scoreOffset(self.scoreDict[self.keys[idx]]!,
+                                            scoreHeight: sHeight)
+          let scoreStop = self.scoreOffset(self.scoreDict[self.keys[idx + 1]]!,
+                                           scoreHeight: sHeight)
+          let startPoint = CGPoint(x: dStart,
+                                   y: reader.size.height - scoreStart)
+          let endPoint = CGPoint(x: dStop,
+                                 y: reader.size.height - scoreStop)
           p.move(to: startPoint)
           p.addLine(to: endPoint)
         }
