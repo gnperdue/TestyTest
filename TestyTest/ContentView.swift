8d9387befd3d16f3cb290fc2f81b0627451191e4
Gabriel Perdue <gnperdue@me.com>
2020-05-26 19:15:15 -0500

    draw some super simple marks

--- TestyTest/ContentView.swift
+++ TestyTest/ContentView.swift
@@ -10,8 +10,23 @@ import SwiftUI
 
 
 struct ContentView: View {
+  let xs: [CGFloat] = [1.0, 2.0, 3.0]
+  let ys: [CGFloat] = [1.0, 2.0, 3.0]
+  
   var body: some View {
-    Text("Mellow")
+    GeometryReader { reader in
+      ForEach(0..<3) { idx in
+        Path { p in
+          let xScaleFactor = reader.size.width / 10.0
+          let yScaleFactor = reader.size.height / 10.0
+          p.move(to: CGPoint(x: self.xs[idx] * xScaleFactor,
+                             y: self.ys[idx] * yScaleFactor))
+          p.addLine(to: CGPoint(x: (self.xs[idx] + 0.5) * xScaleFactor,
+                                y: self.ys[idx] * yScaleFactor))
+        }
+        .stroke()
+      }
+    }
   }
 }
 
