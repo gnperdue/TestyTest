64ee158556f194ba511c674245281e93b4de7f4a
Gabriel Perdue <gnperdue@me.com>
2020-06-10 06:30:02 -0500

    add a stub view file for the chart

---
+++ TestyTest/ScoresChartView.swift
@@ -0,0 +1,21 @@
+//
+//  ScoresChartView.swift
+//  TestyTest
+//
+//  Created by Gabriel Perdue on 6/10/20.
+//  Copyright © 2020 Gabriel Perdue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScoresChartView: View {
+  var body: some View {
+    Text("Mellow, whirled...")
+  }
+}
+
+struct ScoresChartView_Previews: PreviewProvider {
+  static var previews: some View {
+    ScoresChartView()
+  }
+}
