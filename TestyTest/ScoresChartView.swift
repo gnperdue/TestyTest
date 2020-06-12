747604a4117ab76eb1324e7bedf08d0904e7e511
Gabriel Perdue <gnperdue@me.com>
2020-06-12 06:09:30 -0500

    attempt to draw axes and ticks with separate GeometryReaders with hilarious results

--- TestyTest/ScoresChartView.swift
+++ TestyTest/ScoresChartView.swift
@@ -9,38 +9,108 @@
 import SwiftUI
 
 struct ScoresChartView: View {
+  let verticalPaddingFraction: CGFloat = 0.05
+  let horizontalPaddingFraction: CGFloat = 0.05
+  let highScore = 50
+  let maxScoreDigits = 3
+  let nYTicks = 10
+  let nXTicks = 10  // TODO - distribute dates...
+
   var body: some View {
-    Text("Mellow, whirled...")
+    Group {
+      GeometryReader(content: drawXYAxis(with:))
+      GeometryReader(content: drawYAxisTicks(with:))
+    }
   }
   
-  func drawXAxis(with reader: GeometryProxy) -> some View {
-    let verticalPaddingFraction: CGFloat = 0.05
-    let horizontalPaddingFraction: CGFloat = 0.05
+  func bottomLeadingCorner(with reader: GeometryProxy) -> CGPoint {
+    return CGPoint(
+      x: reader.size.width * horizontalPaddingFraction,
+      y: reader.size.height * (1 - verticalPaddingFraction))
+  }
+  
+  func bottomTrailingCorner(with reader: GeometryProxy) -> CGPoint {
+    return CGPoint(
+      x: reader.size.width * (1 - horizontalPaddingFraction),
+      y: reader.size.height * (1 - verticalPaddingFraction))
+  }
+  
+  func topLeadingCorner(with reader: GeometryProxy) -> CGPoint {
+    return CGPoint(
+      x: reader.size.width * horizontalPaddingFraction,
+      y: reader.size.height * verticalPaddingFraction)
+  }
+  
+  // try to make this work for x and y-axes
+  func tickPos(
+    dimension: CGFloat, padding: CGFloat, nTicks: Int, tick: Int
+  ) -> CGFloat {
+    let verticalSpan = dimension - padding
+    let tickStep = verticalSpan / CGFloat(nTicks)
+    let pos = dimension - padding / 2.0 - CGFloat(tick) * tickStep
+    return pos
+  }
 
-    let bottomLeadingCorner = CGPoint(
-        x: reader.size.width * horizontalPaddingFraction,
-        y: reader.size.height * (1 - verticalPaddingFraction))
-    let bottomTrailingCorner = CGPoint(
-        x: reader.size.width * (1 - horizontalPaddingFraction),
-        y: reader.size.height * (1 - verticalPaddingFraction))
-    let topLeadingCorner = CGPoint(
-        x: reader.size.width * horizontalPaddingFraction,
-        y: reader.size.height * verticalPaddingFraction)
+  // try to make this work for x and y-axes
+  func tickLabelPos(
+    dimension: CGFloat, padding: CGFloat, nTicks: Int, tick: Int
+  ) -> CGFloat {
+    let verticalSpan = dimension - padding
+    let tickStep = verticalSpan / CGFloat(nTicks)
+    let pos = dimension - padding / 2.0 - CGFloat(tick) * tickStep -
+      tickStep / 3.5
+    return pos
+  }
+  
+  func tickLabel(highScore: Int, nTicks: Int, tick: Int) -> String {
+    let marker: Double = Double(highScore) -
+      Double(nTicks - tick) * Double(highScore) / Double(nTicks)
+    let label = String(format: "%.0f", marker)
+    // wherefore art thou, o' built-in String padding functions?...
+    return String(
+      repeating: " ", count: self.maxScoreDigits - label.count) + label
+  }
 
-    return Group {
-        Path { p in
-            p.move(to: bottomLeadingCorner)
-            p.addLine(to: bottomTrailingCorner)
-        }.stroke(Color.black, lineWidth: 5.0)
-        Path { p in
-            p.move(to: bottomLeadingCorner)
-            p.addLine(to: topLeadingCorner)
-        }.stroke(Color.black, lineWidth: 5.0)
-        Path { p in
-            p.move(to: topLeadingCorner)
-            p.addLine(to: bottomTrailingCorner)
-        }.stroke(Color.black, lineWidth: 5.0)
+  func drawXYAxis(with reader: GeometryProxy) -> some View {
+    let bottomLeading = bottomLeadingCorner(with: reader)
+    let bottomTrailing = bottomTrailingCorner(with: reader)
+    let topLeading = topLeadingCorner(with: reader)
+
+    return Path { p in
+      p.addLines([topLeading, bottomLeading, bottomTrailing])
     }
+    .stroke(Color.black, lineWidth: 5.0)
+  }
+  
+  func drawYAxisTicks(with reader: GeometryProxy) -> some View {
+    ForEach(0..<nYTicks) { tick in
+      Group {
+        Path { p in
+          let yPos = self.tickPos(
+            dimension: reader.size.height,
+            padding: self.verticalPaddingFraction * reader.size.height,
+            nTicks: self.nYTicks,
+            tick: tick)
+          let tickStart = CGPoint(
+            x: self.horizontalPaddingFraction * reader.size.width / 2.0 - self.horizontalPaddingFraction * reader.size.width / 4.0,
+            y: yPos)
+          let tickStop = CGPoint(
+            x: self.horizontalPaddingFraction * reader.size.width / 2.0,
+            y: yPos)
+          p.move(to: tickStart)
+          p.addLine(to: tickStop)
+        }
+        .stroke(Color.black, lineWidth: 5.0)
+        Text("\(self.tickLabel(highScore: self.highScore, nTicks: self.nYTicks, tick: tick))")
+          .offset(x: self.horizontalPaddingFraction * reader.size.width,
+                  y: self.tickLabelPos(
+                    dimension: reader.size.height,
+                    padding: self.verticalPaddingFraction * reader.size.height,
+                    nTicks: self.nYTicks,
+                    tick: tick))
+      }
+    }
+
   }
 }
 
